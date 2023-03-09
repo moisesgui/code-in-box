3b7e01836fcc1a693d9967425444a97f8c64bc85
moisesgui <mgoliveira.dev@gmail.com>
2023-03-09 16:33:42 -0300

    [feat]Product-card created

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,120 +1,57 @@
 import React from "react";
-import logo from '../../assets/imagens/logo01.png'
+
+
+import logoCode from '../../assets/imagens/logo01.png';
+import burgerMenu from '../../assets/imagens/burger-menu.png';
+import { ProductCard } from "../../shared/components/productCard/ProductCard";
 import './Home.scss';
 
-
 export const Home = () => {
-
   return (
-    <div className="flex flex-col h-screen">
-      {/* Header */}
-      <header className="bg-blue text-gray-50 shadow-xl">
-        <div className="container flex flex-wrap mx-auto p-4 justify-between flex-col md:flex-row items-center">
-          <a href="#"  className="flex title-font font-medium items-center text-white mb-4 md:mb-0 cursor-pointer">
-            <img src={logo} alt="code-logo" width={220} />
+    <div className="home-container">
+      <header>
+        <div className="header__content">
+          <div class="header__logo-container">
+          <button href="#"  className="md:hidden js-burgerMenu">
+            <img src={burgerMenu} alt="code-logo" width={36} />
+          </button>
+          <a href="#"  className="header_content--logo">
+            <img src={logoCode} alt="code-logo" width={220} />
           </a>
-          <form className="">
+          </div>
+          <form>
             <input
-              className="bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mr-4 w-full md:w-auto"
-              placeholder="Search"
+              placeholder="Buscar..."
               type="text"
             />
-            <button className="inline-flex items-center text-blue font-semibold bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-200 rounded-lg">
-              Search
+            <button className="search-button">
+              Buscar
             </button>
           </form>
-          <nav className="">
+          <nav className="hidden md:flex">
             <a href="#" className="mr-5 hover:text-white">Home</a>
-            <a href="#" className="mr-5 hover:text-white">About</a>
-            <a href="#" className="mr-5 hover:text-white">Contact</a>
+            <a href="#" className="mr-5 hover:text-white">Sobre</a>
+            <a href="#" className="mr-5 hover:text-white">Contato</a>
           </nav>
         </div>
       </header>
 
-      {/* Content */}
-      <div className="flex-grow">
-        <div className="container mx-auto px-4 pt-16 lg:pt-24 pb-8">
-          <h2 className="text-4xl font-bold mb-8">Welcome to My Site</h2>
-          <p className="text-xl mb-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
+      <section>
+          <div className="hero">
+              <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+              <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+              <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-yellow-500 bg-blue rounded-lg hover:bg-yellow-600">
+                  Learn more
+                  <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              </a>
+          </div>
+      </section>
 
-          </p>
+      <section>
+        <div className="product-card-container">
+          <ProductCard />
         </div>
-        <div className="container mx-auto px-4 pt-16 lg:pt-24 pb-8">
-          <h2 className="text-4xl font-bold mb-8">Welcome to My Site</h2>
-          <p className="text-xl mb-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            
-          </p>
-        </div>
-        <div className="container mx-auto px-4 pt-16 lg:pt-24 pb-8">
-          <h2 className="text-4xl font-bold mb-8">Welcome to My Site</h2>
-          <p className="text-xl mb-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            rhoncus eleifend nisi, vitae dictum nisi facilisis at. Donec eget
-            libero leo. Donec pretium urna vel sem lobortis ullamcorper.
-            
-          </p>
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
