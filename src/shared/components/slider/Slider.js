7030c3b811255e0583d6aac61ffc7b867075e91e
moisesgui <mgoliveira.dev@gmail.com>
2023-03-09 23:46:59 -0300

    slick slider add

---
+++ src/shared/components/slider/Slider.js
@@ -0,0 +1,49 @@
+import Slider from "react-slick";
+import { ProductCard } from "../productCard/ProductCard";
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+export const Carrousel = () => {
+
+  const settings = {
+    className: "slider variable-width",
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: 350 }}>
+          <ProductCard />
+        </div>
+      </Slider>
+    </div>
+  );
+}
